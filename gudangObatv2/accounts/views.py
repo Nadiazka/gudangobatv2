0bb632c7963e7d6f5ce191425a9229b2568a431d
Nadiazka <nadiaazkahp@yahoo.com>
2021-01-02 15:31:39 +0700

    Update 2 Jan v4

--- gudangObatv2/accounts/views.py
+++ gudangObatv2/accounts/views.py
@@ -47,7 +47,7 @@ def loginPage(request):
 
 		if user is not None:
 			login(request, user)
-			return redirect('home')
+			return redirect('$')
 		else:
 			messages.info(request, 'Username atau Password salah')
 
