258b3b7dc9715d2db53b25f2c0310afec770b28c
Nadiazka <nadiaazkahp@yahoo.com>
2021-01-03 00:24:37 +0700

    Remove duplicated directory

--- gudangObatv2/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from accounts import views
-
-urlpatterns = [
-    url(r'^login/', views.loginPage, name="login"),
-    url(r'^register/', views.registerPage, name="register"),
-    url(r'^logout/', views.logoutUser, name="logout"),
-]
--- gudangObatv2/gudangObatService/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GudangobatserviceConfig(AppConfig):
-    name = 'gudangObatService'
